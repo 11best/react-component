80b2dd1b058407965487f6f67eb6868febe94650
11best <asama_b@outlook.com>
2023-06-05 00:00:03 +0700

    set layout for header

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,10 +3,10 @@ import Header from "./components/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="max-w-3xl mx-auto">
       <Header></Header>
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,17 @@
 const Header = () => {
   return (
-    <header className="bg-red-300">
-      <p>Title</p>
+    <header className="h-12 bg-red-300 flex justify-between items-center">
+      <div className=" bg-yellow-200 w-20 h-12 flex items-center justify-center">
+        Title
+      </div>
+      <div>
+        <a href="" className="font-semibold hover:text-amber-500 mx-4">
+          page 1
+        </a>
+        <a href="" className="font-semibold hover:text-amber-500 mx-4">
+          page 2
+        </a>
+      </div>
     </header>
   );
 };
