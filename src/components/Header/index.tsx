3e793dd80684e2477ac55ace7e56656ac60d84a1
11best <asama_b@outlook.com>
2023-06-05 21:29:13 +0700

    create hamburger for mobile view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,68 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+type HeaderItem = {
+  name: string;
+  url: string;
+};
+
+const headerItems: HeaderItem[] = [
+  {
+    name: "page 1",
+    url: "",
+  },
+  {
+    name: "page 2",
+    url: "",
+  },
+];
+
 const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <header className="h-12 bg-red-300 flex justify-between items-center">
-      <div className=" bg-yellow-200 w-20 h-12 flex items-center justify-center">
-        Title
+    <header className="h-12 bg-red-300">
+      <div className="hidden sm:flex justify-between items-center">
+        <div className=" bg-yellow-200 w-20 h-12 flex items-center justify-center">
+          Title
+        </div>
+        <div>
+          {headerItems.map((item) => (
+            <a
+              key={item.name}
+              href={item.url}
+              className="font-semibold hover:text-amber-500 mx-4"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
       </div>
-      <div>
-        <a href="" className="font-semibold hover:text-amber-500 mx-4">
-          page 1
-        </a>
-        <a href="" className="font-semibold hover:text-amber-500 mx-4">
-          page 2
-        </a>
+
+      <div className="sm:hidden absolute right-0">
+        <button
+          onClick={() => setExpanded(!expanded)}
+          className="bg-amber-200 p-2 m-1 hover:bg-amber-300 rounded"
+        >
+          ham
+        </button>
+        <div
+          className={clsx(
+            expanded ? "absolute right-0 bg-amber-200 w-64" : "hidden"
+          )}
+        >
+          <div className="flex flex-col">
+            {headerItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.url}
+                className="font-semibold hover:text-amber-500 my-2 ml-8 w-fit"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </header>
   );
